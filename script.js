8481741a6a0b5e6c2503edc0c16f94087a3ce3be
Sergio <42003746+maneaterr2@users.noreply.github.com>
2020-06-03 16:21:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -73,8 +73,71 @@
         let cur_time = time.getHours()+":"+ time.getMinutes()+":"+ time.getSeconds()+":"+ millisec;
         ctx.fillText(cur_time, (centerX - 160), centerY);
     }
+    const rect_count = 60;
+    let rect_arr =[0];
+    let rectY_coord = 0;
+
+    function falling_rect(){
+        if(rectY_coord>(cnv.height + 25)){
+            rectY_coord = rectY_coord - 25;
+            // rect_arr.push(random_str());
+            rect_arr.shift();
+           
+        }else{
+            if((rectY_coord%25)===0){
+                rect_arr.push(random_str());
+            }
+            rectY_coord +=25;
+        }
+        draw_rect()
+    }
+
+    function draw_rect(){
+        let x = cnv.width / (rect_count +1);
+        
+            for(let index = 0;index<rect_arr.length;index++){
+
+                for(let i=0;i<rect_arr[index].length;i++){
+                    ctx.fillStyle = "#429e44";
+                    let word = rect_arr[index][i];
+                    if(word=="電"){
+                        ctx.fillStyle = "#96c997";
+                    }
+                    ctx.font = "20px Arial";
+                    ctx.textAlign = "center";
+                    let curX = x*(i+1);
+                    let curY = rectY_coord - (index*25);
+                    // ctx.fillRect(curX, curY, 5, 5);
+                    ctx.fillText(word, curX, curY);
+                    // rect_arr.push("1");
+                }
+                // ctx.fillStyle = "#429e44";
+                // ctx.font = "60px Arial";
+                // ctx.textAlign = "center";
+                // ctx.fillText(rectY_coord+" "+rect_arr.length, centerX, (centerY - 100));
+            }
+       
+        
+       
+    }
+    function random_sign(){
+        // var alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        var alphabet ="送 料   無 料       !格 安    オリジナル 秒     針 携    帯 電   話";
+        var randomIndex = Math.floor(Math.random() * alphabet.length);
+
+        var randomLetter = alphabet[randomIndex];
+        return randomLetter;
+    }
+    function random_str(){
+        let arr = [];
+        for(let i=0;i<rect_count;i++){
+            arr.push(random_sign());
+        }
+        return arr;
+    }
     function loop(){
         cnv.width |=0; //ctx.clearRect(0,0, cnv.width,cnv.height)// clear space
+        falling_rect();
         updateRings();
         time_now();
         requestAnimationFrame(loop);
